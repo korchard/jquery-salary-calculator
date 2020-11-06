67d08d9a419faef8ff37bbd4a277e9dfea85805a
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2020-11-06 15:37:59 -0600

    created event handler for submit button, as well as function pushing info into the list

--- client.js
+++ client.js
@@ -1,8 +1,19 @@
-console.log('in javaScript');
+console.log('in javaScript'); // ensure javaScript is working
 
 $(document).ready(onReady);
 
 function onReady() {
-    console.log('in jQuery');
+    console.log('in jQuery'); // ensure jQuery is working
+    
+    $('#submitButton').on('click', submitInfo); // event handler to add info to DOM
 }
 
+function submitInfo() {
+    let firstName = $('#employeeFirstName').text();
+    let lastName = $('#employeeLastName').text();
+    let idNumber = $('#employeeIDNumber').val();
+    let jobName = $('#jobTitle').text();
+    let annualSal = $('#annualSalary').val();
+
+    $('.listOfEmployees').append(`<li></li>`)
+}
